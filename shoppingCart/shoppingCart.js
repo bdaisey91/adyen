a705fc2dda85542329f0666538e14e80870fce29
Daisey Bell <bdaisey91@gmail.com>
2019-12-04 20:50:23 -0800

    use protocol-specific originKey

--- shoppingCart/shoppingCart.js
+++ shoppingCart/shoppingCart.js
@@ -22,7 +22,16 @@ function launchDropIn() {
     })
   })
   paymentMethods = await paymentMethods.json()
-  const originKey = (window.origin === 'http://localhost:8082') ? 'pub.v2.8115532860023116.aHR0cDovL2xvY2FsaG9zdDo4MDgy.tm_BCb0ekshyP1rn4GtET9aJXI9u1IXyCFdVlVGNl7M' : 'pub.v2.8115532860023116.aHR0cDovL2FkeWVuLXRlY2gtc3VwcG9ydC10ZXN0Lmhlcm9rdWFwcC5jb20v.BIooMyHbfffkwhsipbY_NnXXaqNlRsLtX0jKKLn7N78'
+  const originKey = (() => {
+    if (window.origin === 'http://localhost:8082') {
+      return 'pub.v2.8115532860023116.aHR0cDovL2xvY2FsaG9zdDo4MDgy.tm_BCb0ekshyP1rn4GtET9aJXI9u1IXyCFdVlVGNl7M'
+    }
+    if (window.location.protocol === 'https:') {
+      return 'pub.v2.8115532860023116.aHR0cHM6Ly9hZHllbi10ZWNoLXN1cHBvcnQtdGVzdC5oZXJva3VhcHAuY29tLw.Kfb11liW4wA4ivHg-o5YAfgvDCws-6M6shU1C72veXc'
+    } else {
+      return 'pub.v2.8115532860023116.aHR0cDovL2FkeWVuLXRlY2gtc3VwcG9ydC10ZXN0Lmhlcm9rdWFwcC5jb20v.BIooMyHbfffkwhsipbY_NnXXaqNlRsLtX0jKKLn7N78'
+    }
+  })()
   const configuration = {
     locale: 'en-NL',
     environment: 'test',
